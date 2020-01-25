f8607bf85362becc47e84f6059499e94285bf58a
renee <renee@renees-MacBook-Pro.local>
2020-01-26 02:13:03 +1000

    update gatsby version

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,8 @@
 module.exports = {
   siteMetadata: {
     title: `StudyNote`,
+    description: `A simple study blog site.`,
+    author: `Renee LIN`,
   },
   plugins: [
     {
@@ -37,13 +39,13 @@ module.exports = {
         name: `GatsbyJS`,
         short_name: `GatsbyJS`,
         start_url: `/`,
-        background_color: `#6b37bf`,
-        theme_color: `#6b37bf`,
-        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
-        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        background_color: `#f7f0eb`,
+        theme_color: `#a2466c`,
         display: `standalone`,
-        icon: `src/images/icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/icon.png`,
       },
     },
+    `gatsby-plugin-offline`,
+    `gatsby-plugin-react-helmet`,
   ],
 }
